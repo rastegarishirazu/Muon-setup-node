6778ef84bd54bd006feff8c97976bdd0afa85b79
ali aliakbari <rastegari1375@gmail.com>
2023-01-23 18:02:55 +0330

    change pubpath to join

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,6 @@
 const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
-  publicPath: "/",
+  publicPath: "/join/",
   // publicPath: process.env.NODE_ENV === "production" ? "/join/" : "/",
   transpileDependencies: ["vuetify"],
   pwa: {
