79a0bddc716d17a4d7017ed3a62de44ec38831c6
ali aliakbari <rastegari1375@gmail.com>
2023-01-25 19:01:14 +0330

    change PP

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,6 @@
 const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
-  // publicPath: "./",
-  publicPath: process.env.NODE_ENV === "production" ? "/test/" : "",
+  publicPath: "/test/",
+  // publicPath: process.env.NODE_ENV === "production" ? "/test/" : "",
   transpileDependencies: ["vuetify"],
 });
