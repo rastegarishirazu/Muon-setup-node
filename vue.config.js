4a9ce9506d8df8af9ee0ea17fc6e1a0c57093821
ali aliakbari <rastegari1375@gmail.com>
2023-01-25 19:25:13 +0330

    change publicPath to join

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,6 @@
 const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
-  publicPath: "/test/",
+  publicPath: "/join/",
   // publicPath: process.env.NODE_ENV === "production" ? "/test/" : "",
   transpileDependencies: ["vuetify"],
 });
