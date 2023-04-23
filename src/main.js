3974590373c358d4e9226256cea37115f51b1567
unknown <rastegari1375@gmail.com>
2023-04-23 13:55:45 +0330

    add route

--- src/main.js
+++ src/main.js
@@ -4,6 +4,7 @@ import "./registerServiceWorker";
 import vuetify from "./plugins/vuetify";
 import LottieVuePlayer from "@lottiefiles/vue-lottie-player";
 import { createPinia, PiniaVuePlugin } from "pinia";
+import router from './router'
 
 Vue.use(LottieVuePlayer); // add lottie-animation to your global scope
 Vue.use(PiniaVuePlugin);
@@ -13,7 +14,9 @@ Vue.config.productionTip = false;
 new Vue({
   vuetify,
   pinia,
+  router,
+
   render: function (h) {
     return h(App);
-  },
+  }
 }).$mount("#app");
